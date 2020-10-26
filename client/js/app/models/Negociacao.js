98b5e95676f016d5c80113ab8f86a007420993da
Raul Franck <raulengfranck@gmail.com>
2020-10-25 23:18:07 -0300

    implementando controller

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,39 +2,29 @@ class Negociacao {
 
   constructor(data, quantidade, valor) {
 
-    this._data = data;
+    this._data = new Date(data.getTime());
     this._quantidade = quantidade;
     this._valor = valor;
+    Object.freeze(this);
   }
 
   get volume() {
+
     return this._quantidade * this._valor;
   }
 
   get data() {
-    return this._data;
+
+    return new Date(this._data.getTime());
   }
 
   get quantidade() {
+
     return this._quantidade;
   }
 
   get valor() {
+
     return this._valor;
-  }
-}
-
-
-
-class Pessoa {
-  constructor(nome, sobrenome) {
-
-
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-  }
-
-  get obterNomeCompleto() {
-    return this.nome + ' ' + this.sobrenome;
   }
 }
